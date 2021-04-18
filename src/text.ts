638705b7b11fe3e1fbf839f76c974f0eea721d9e
Brigosha <pranav.dixit@brigosha.com>
2021-04-18 14:44:12 +0530

    final commit

--- src/text.ts
+++ src/text.ts
@@ -1,3 +1,25 @@
-//http://localhost:5001/subscription/create?phone=8105130481&productId=2 - post
-//http://localhost:5001/subscription/updateProduct?phone=8105130481&productId=1 -put
-//http://localhost:5001/subscription/updateSubscription?phone=8105130481&status=Inactive - put
+//http://localhost:5001/subscription/create?phone=8105130481&productId=2 - post (create subscription)
+//http://localhost:5001/subscription/updateProduct?phone=8105130481&productId=1 -put (Update product)
+//http://localhost:5001/subscription/updateSubscription?phone=8105130481&status=Inactive - put (Cancel subscription)
+
+
+// git repo :- https://github.com/Pranavd91/ecommerce.git
+// branch master
+
+// https://cloud.mongodb.com/ for cloud db
+// username:- dixit.pranav07@gmail.com
+// password :- 194mongodb@
+
+// click on the collection
+
+
+// Hi it is basic crud example ecommerce demo
+// i can do it by SQL db also in which it requires mapping relation also 
+// like one- to- one and one -to - many , but i have some time constraint 
+
+// how to run project :
+// clone the above git link
+// then run in terminal :- npm i 
+// npm i mongodb , class-validator
+
+// after installing all the dependencies run : npm run start:dev
